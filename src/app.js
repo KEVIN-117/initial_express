13b0d1ea6e3ec25d492530ab1fadeacb59cd65d0
Kevin Alexis Rodriguez Condori <sis414.kevinrodriguez17@gmail.com>
2023-06-23 18:34:25 +1300

    production

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
 import express from 'express';
-import cors from 'cors'
-import productRouter from './router/product.routes.js'
-import userRoutes from './router/user.routes.js'
+import cors from 'cors';
+import productRouter from './router/product.routes.js';
+import userRoutes from './router/user.routes.js';
 
-import { errorHandler, logError, boomHandler } from './middleware/example.middleware.js'
+import {
+  errorHandler,
+  logError,
+  boomHandler,
+} from './middleware/example.middleware.js';
 const app = express();
 
+app.use(express.json()); // json() es un middleware
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
+app.use('/api', productRouter);
+app.use('/api', userRoutes);
 
-app.use(cors({
-  origin: 'http://localhost:3001'
-}));
-app.use(express.json()) // json() es un middleware
-app.use('/api', productRouter)
-app.use('/api', userRoutes)
+app.use(logError);
+app.use(boomHandler);
+app.use(errorHandler);
 
-app.use(logError)
-app.use(boomHandler)
-app.use(errorHandler)
-
-
-
-export default app
+export default app;
